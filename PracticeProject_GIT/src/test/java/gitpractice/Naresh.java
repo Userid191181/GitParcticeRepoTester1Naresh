af87674e3170a08bd51a24a9d1349ee51af47dab
Userid191181 <cnaresh4567@gmail.com>
2025-11-22 10:35:38 +0530

    Update Naresh.java

--- PracticeProject_GIT/src/test/java/gitpractice/Naresh.java
+++ PracticeProject_GIT/src/test/java/gitpractice/Naresh.java
@@ -4,9 +4,11 @@ public class Naresh {
 
 	public static void main(String[] args) {
 	System.out.println("TEsting today saturday");
+		System.out.println("TEsting today second saturday");
 
 	}
 
 }
+
 
 
